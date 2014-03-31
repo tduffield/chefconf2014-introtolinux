8dd572cf0fd66c0ee922b4315f0ab881b04f2372
Tom Duffield <tom@opscode.com>
2014-03-31 13:33:29 -0500

    slide 244 - replaced data bag with search, added roles

--- cookbooks/apache/recipes/vhosts.rb
+++ cookbooks/apache/recipes/vhosts.rb
@@ -1,6 +1,5 @@
 
-data_bag("vhosts").each do |site|
-  site_data = data_bag_item("vhosts", site)
+search(:vhosts).each do |site_data|
   site_name = site_data['id']
   document_root = "/srv/apache/#{site_name}"
 
